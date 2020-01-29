e75db7c1f50bd0c57a41c177748291e24952467a
Kaiisha05 <Kaiisha@yahoo.com>
2020-01-29 00:28:03 -0500

    added header code from Jon's repo

--- tis-the-seasoning/src/components/Header/Header.js
+++ tis-the-seasoning/src/components/Header/Header.js
@@ -2,16 +2,29 @@ import React from 'react';
 import './Header.css';
 
 
-function Header() {
+// function Header() {
+//   return (
+//     <div className="row">
+//       <div className="medium-12 columns">
+//   <h2>Header Page</h2>
+//       </div>
+//     </div>
+//   );
+// }
+
+const Header = () => {
   return (
-    <div className="row">
-      <div className="medium-12 columns">
-  <h2>Header Page</h2>
-      </div>
-    </div>
+    <header className="jumbotron text-center mb-0">
+      <h1 className="display-4">
+        {/* <Link to="/">‘Tis the Seasoning</Link> */}
+        Tis the Seasoning
+      </h1>
+      <p className="container my-4">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
+        voluptatum quidem harum.
+      </p>
+    </header>
   );
-}
-
-
+};
 
 export default Header;
