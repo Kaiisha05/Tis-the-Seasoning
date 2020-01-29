13f4947554d722884c886e5925e21ec3e0ce42d6
Kaiisha05 <Kaiisha@yahoo.com>
2020-01-29 12:12:11 -0500

    updated comments for routing

--- tis-the-seasoning/src/App.js
+++ tis-the-seasoning/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//as = allows me to change the name of BrowserRouter to something shorter and easier to work with.
+
 import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
 import Home from './components/Home/Home';
@@ -7,17 +9,17 @@ import Login from './components/Login/Login';
 import Recipe from './components/Recipe/Recipe';
 import AllRecipes from './components/AllRecipes/AllRecipes';
 import NotFound from './components/NotFound/NotFound';
-import './styles/foundation.min.css';
 import './styles/custom.css';
 
 
 function App() {
   return (
     <Router>
-
+{/* Router = wraps all your components that you want to have the ability to use routing */}
       <div className='App'>
         <Header />
         <Switch>
+          {/* Switch = stops the whole process of going through all the routes. As soon as the path matches the URL it wll stop and only render out that specific component. You will have to include "exact" for the "home" route so it won't stop on this component it will keep checking */}
           <Route exact path='/' component={Home} />
           <Route path='/home' component={Home} />
           <Route path='/welcome' component={Welcome} />
--- tis-the-seasoning/src/components/Welcome/Welcome.js
+++ tis-the-seasoning/src/components/Welcome/Welcome.js
@@ -4,13 +4,59 @@ import './Welcome.css';
 
 function Welcome() {
   return (
-    <div className="container"> 
-       <h2>A Recipe-Sharing App. Test new flavors or impress by sharing one of your own...</h2>
+    <div>
+    <div className="container">
 
-       <button>Sign In</button> 
-       <button>Login</button>
+      <div className="container" />
+      <div className="row justify-content-between" />
+      <div className="col-5" />
+
+      <form className="form-group" action="/login" method="POST" />
+      <div>
+        <h2>Welcome Back!</h2>
+        <label for="email">Email</label>
+        <input type="email" className="form-control" id="email" name="email" required />
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <input type="password" className="form-control" id="password" name="password" required />
+      </div>
+      <button className="btn btn-danger" type="submit">Login</button>
+
     </div>
-  );
+
+    <div className="col-5" />
+
+    <form className="form-group" action="/register" method="POST" />
+    <h2>Come Taste!</h2>
+    <div>
+      <label for="username">User Name</label>
+      <input type="text" className="form-control" id="username" name="username" required />
+    </div>
+    <div>
+      <label for="lastname">Last Name</label>
+      <input type="text" className="form-control" id="lastname" name="lastname" required />
+    </div>
+    <div>
+      <label for="firstname">First Name</label>
+      <input type="text" className="form-control" id="firstname" name="firstname" required />
+    </div>
+    <div>
+      <label for="email">Email</label>
+      <input type="email" className="form-control" id="email" name="email" required />
+    </div>
+    <div>
+      <label for="password">Password</label>
+      <input type="password" className="form-control" id="password" name="password" required />
+    </div>
+
+    <button className="btn btn-danger" type="submit">Register</button>
+
+           
+        </div >
+    
+    
+    );
 }
 
 
